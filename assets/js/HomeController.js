4b0ac65af5e11bac44c84cd9b2be9e33d728f852
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-02-17 18:44:19 -0500

    animating home page

--- assets/js/HomeController.js
+++ assets/js/HomeController.js
@@ -2,9 +2,26 @@
  * Created by STO-SD003 on 2017-01-23.
  */
 
-var HomeController = {
+const HomeController = {
     init: function () {
+        let collection = ["watch", "look", "listen"];
+        jQuery(".filter-buttons").on("click", function(e){
+            let button = e.target;
+            jQuery.each(collection, function(index, value){
 
+                if(value != button.id){
+                    jQuery("." + value).hide(1000);
+                }else{
+                    jQuery("." + value).show(1000);
+                }
+            });
+
+        });
+        jQuery(".viewport, .viewport-half").hover(function() {
+      jQuery(".title, .subtitle").animate({ color: "#fff" }, 1000);
+    },function() {
+      jQuery(".title, .subtitle").animate({ color: "#000" }, 1000);
+    });
 
     },
 };
--- static/assets/bundles/main-311cb9a318e50da9ada4.js
+++ static/assets/bundles/main-311cb9a318e50da9ada4.js
@@ -12766,8 +12766,26 @@ var DaisController =
 	 * Created by STO-SD003 on 2017-01-23.
 	 */
 	
-	var HomeController = {
-	  init: function () {}
+	const HomeController = {
+	    init: function () {
+	        let collection = ["watch", "look", "listen"];
+	        jQuery(".filter-buttons").on("click", function (e) {
+	            let button = e.target;
+	            jQuery.each(collection, function (index, value) {
+	
+	                if (value != button.id) {
+	                    jQuery("." + value).hide(1000);
+	                } else {
+	                    jQuery("." + value).show(1000);
+	                }
+	            });
+	        });
+	        jQuery(".viewport, .viewport-half").hover(function () {
+	            jQuery(".title, .subtitle").animate({ color: "#fff" }, 1000);
+	        }, function () {
+	            jQuery(".title, .subtitle").animate({ color: "#000" }, 1000);
+	        });
+	    }
 	};
 	
 	module.exports = HomeController.init();
@@ -12797,4 +12815,4 @@ var DaisController =
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=main-eda11fb7b15aeb47524b.js.map
+//# sourceMappingURL=main-311cb9a318e50da9ada4.js.map
