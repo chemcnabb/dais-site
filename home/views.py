bf9b510cbcd2bc6bc3b04a521765ee56588992f1
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-02-20 23:23:11 -0500

    test

--- home/views.py
+++ home/views.py
@@ -4,13 +4,13 @@ from watch.models import WatchPost
 from look.models import LookPost
 from listen.models import ListenPost
 # Create your views here.
-
+from itertools import chain
 
 class HomeView(TemplateView):
     template_name = "home.html"
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context["post_list"] = [WatchPost.objects.all(),LookPost.objects.all(), ListenPost.objects.all()]
+        context["post_list"] = list(chain(WatchPost.objects.all(),LookPost.objects.all(), ListenPost.objects.all()))
 
 
         return context
