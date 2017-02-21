442affd19300118cb87890c135ce7c92fddbf1e6
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-02-21 03:26:54 -0500

    migrating db's

--- watch/admin.py
+++ watch/admin.py
@@ -3,4 +3,5 @@ from watch.models import WatchPost
 # Register your models here.
 class WatchAdmin(admin.ModelAdmin):
     save_as=True
+    list_display = ('title',)
 admin.site.register(WatchPost, WatchAdmin)
