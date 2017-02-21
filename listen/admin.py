89248d9382177e4b5946d352fad5696d48d53536
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-02-21 03:28:09 -0500

    migrating db's

--- listen/admin.py
+++ listen/admin.py
@@ -3,4 +3,5 @@ from listen.models import ListenPost
 # Register your models here.
 class ListenAdmin(admin.ModelAdmin):
     save_as = True
+    list_display = ('title',)
 admin.site.register(ListenPost, ListenAdmin)
