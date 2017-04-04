5c80d33c3a3a8a490ccab9831e57cb7a3e9deafe
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-04-04 10:08:09 -0400

    resetting js

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from music.models import MusicPost
+# Register your models here.
+class MusicAdmin(admin.ModelAdmin):
+    save_as = True
+    list_display = ('artist_name','order')
+    list_editable = ('order',)
+admin.site.register(MusicPost, MusicAdmin)
