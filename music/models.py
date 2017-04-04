be9afaebc3cfb7f2f35de8ba7f47c3d76474e9db
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-04-04 10:06:37 -0400

    resetting js

--- music/models.py
+++ music/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from tinymce.models import HTMLField
+# Create your models here.
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class MusicPost(models.Model):
+    date = models.DateTimeField()
+    poster_image = models.ImageField(blank=True, null=True)
+    artist_name = models.CharField(max_length=500,blank=True, null=True)
+    bio = HTMLField(blank=True, null=True)
+    soundcloud_embed = models.CharField(max_length=500, blank=True, null=True)
+    order = models.IntegerField(default=0)
+    slug = models.SlugField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.artist_name
+
+    def category(self):
+        return "music"
+
+    def get_url(self):
+        return "/music/%s" % self.slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.artist_name)
+        super(MusicPost, self).save(*args, **kwargs)
+
--- music/views.py
+++ music/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from music.models import MusicPost
 # Create your views here.
 
 
 class MusicView(TemplateView):
     template_name = "music.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(MusicView, self).get_context_data(**kwargs)
+        # context["post_list"] = list(chain(WatchPost.objects.all(),LookPost.objects.all(), ListenPost.objects.all()))
+
+        context["post_list"] = MusicPost.objects.all().order_by('order')
+
+
+        return context
+
+
